45553c6158a1b2f10c66ce05bcd64a8f183a91f5
David Gomez <d.gomezsos97@gmail.com>
2026-02-10 08:21:40 +0100

    Inicio creación de la página de usuario en panel admin

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('admin');
+// Route::get('/admin/usuarios', function () {
+//     return view('admin-usuarios');
+// });
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::resource('usuarios', 'App\Http\Controllers\Admin\UserController', [
+    'parameters' => [
+      'usuarios' => 'user',
+    ],
+    'names' => [
+      'index' => 'users',
+      'create' => 'users_create',
+      'edit' => 'users_edit',
+      'store' => 'users_store',
+      'destroy' => 'users_destroy',
+    ]
+  ]);
 });
