56eb70c8edd78b18d0b9bc6952b6e8b3b88b42e3
David Gomez <d.gomezsos97@gmail.com>
2026-02-10 08:22:59 +0100

    Creación de usuarios al azar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Se crearn 10 registros aleatorios
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
